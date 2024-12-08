813b0c2369e9f6dd6fd69fc8da4a464794f61c56
Mehrdad Rafiee <rafiee.mehrdad@gmail.com>
2024-12-07 16:46:59 -0800

    adds github stars

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,8 @@ import { GitHubLogoIcon } from "@radix-ui/react-icons";
 import { ThemeToggle } from "@/components/ThemeToggle";
 import { KanbanBoard } from "@/components/board/KanbanBoard";
 import Link from "next/link";
+import { ArrowUpRightIcon, StarIcon } from "lucide-react";
+import React from "react";
 
 const FooterLink = ({ children }: { children: React.ReactNode }) => {
   return (
@@ -15,18 +17,37 @@ const FooterLink = ({ children }: { children: React.ReactNode }) => {
 };
 
 function App() {
+  const [starsCount, setStarsCount] = React.useState(0);
+
+  const fetchStarsCount = async () => {
+    const res = await fetch("https://api.github.com/repos/mehrdadrafiee/recursive-dnd-kanban-board");
+    const data = await res.json();
+    setStarsCount(data.stargazers_count);
+  };
+
+  React.useEffect(() => {
+    fetchStarsCount();
+  }, []);
+
   return (
     <div className="min-h-screen flex flex-col">
-      <header className="flex justify-around w-full flex-row p-4 items-center">
-        <Button variant="link" asChild className="text-primary h-8 w-8 p-0">
-          <a href="https://github.com/mehrdadrafiee/recursive-dnd-kanban-board">
-            <GitHubLogoIcon className="fill-current h-full w-full" />
-          </a>
-        </Button>
-        <Button variant="link" asChild className="text-primary h-16 w-16">
-          <a href="https://github.com/mehrdadrafiee">@mehrdadrafiee</a>
-        </Button>
-        <ThemeToggle />
+      <header className="flex justify-around w-full p-4 items-center font-mono">
+        <div>
+          <Button variant="link" asChild className="text-primary h-16 w-16">
+            <a href="https://github.com/mehrdadrafiee">@mehrdadrafiee</a>
+          </Button>
+        </div>
+        <div className="flex items-center justify-end gap-2">
+          <ThemeToggle />
+          <Button variant="outline" asChild>
+            <a href="https://github.com/mehrdadrafiee/recursive-dnd-kanban-board" className="flex items-center">
+              <GitHubLogoIcon className="fill-current h-4 w-4 mr-4" />
+              <StarIcon fill="currentColor" className="h-3 w-3 text-yellow-500 mr-1" />
+              <span className="font-semibold">{starsCount}</span>
+              <ArrowUpRightIcon className="h-4 w-4 ml-4" />
+            </a>
+          </Button>
+        </div>
       </header>
       <main className="mx-4 flex flex-col items-center gap-6 h-[70vh]">
         <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
