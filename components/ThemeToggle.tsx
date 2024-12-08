f9ead1512758f3d6e0e55dfdf2dda5319733fe9e
Mehrdad Rafiee <rafiee.mehrdad@gmail.com>
2024-12-07 16:49:14 -0800

    font-mono for theme toggle

--- components/ThemeToggle.tsx
+++ components/ThemeToggle.tsx
@@ -24,7 +24,7 @@ export function ThemeToggle() {
           <span className="sr-only">Toggle theme</span>
         </Button>
       </DropdownMenuTrigger>
-      <DropdownMenuContent align="end">
+      <DropdownMenuContent align="end" className="font-mono">
         <DropdownMenuItem onClick={() => setTheme("light")}>
           Light
         </DropdownMenuItem>
