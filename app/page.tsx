ed1df3826099eafa40348f673db3e7a5dd3c79b2
Mehrdad Rafiee <rafiee.mehrdad@gmail.com>
2025-04-04 10:24:00 +0300

    adds footer component

--- app/page.tsx
+++ app/page.tsx
@@ -5,14 +5,7 @@ import { KanbanBoard } from "@/components/board/KanbanBoard";
 import Link from "next/link";
 import React from "react";
 import Header from "@/components/Header";
-
-const FooterLink = ({ children }: { children: React.ReactNode }) => {
-  return (
-    <Button variant="link" asChild className="scroll-m-20 text-xl font-semibold tracking-tight">
-      {children}
-    </Button>
-  );
-};
+import Footer from "@/components/Footer";
 
 export default function App() {
   return (
@@ -24,33 +17,7 @@ export default function App() {
         </h1>
         <KanbanBoard />
       </main>
-      <footer className="mt-6 flex flex-col items-center gap-6">
-        <p className="leading-7 [&:not(:first-child)]:mt-6">
-          Heavily inspired by <Link className="underline text-blue-500" href="https://github.com/Georgegriff/react-dnd-kit-tailwind-shadcn-ui">react-dnd-kit-tailwind-shadcn-ui</Link>.✨
-        </p>
-        <ul className="flex items-center justify-center">
-          <li>
-            <FooterLink>
-              <a href="https://nextjs.org/">Next.js</a>
-            </FooterLink>
-          </li>
-          <li>
-            <FooterLink>
-              <a href="https://dndkit.com">DnDKit</a>
-            </FooterLink>
-          </li>
-          <li>
-            <FooterLink>
-              <a href="https://tailwindcss.com/">Tailwind CSS</a>
-            </FooterLink>
-          </li>
-          <li>
-            <FooterLink>
-              <a href="https://ui.shadcn.com/">Shadcn/ui</a>
-            </FooterLink>
-          </li>
-        </ul>
-      </footer>
+      <Footer />
     </div>
   );
 }
---
+++ components/Footer.tsx
@@ -0,0 +1,40 @@
+import Link from "next/link";
+import { Button } from "./ui/button";
+
+const footerLinks = [
+  {
+    label: "Next.js",
+    href: "https://nextjs.org/",
+  },
+  {
+    label: "DnDKit",
+    href: "https://dndkit.com/",
+  },
+  {
+    label: "Tailwind CSS",
+    href: "https://tailwindcss.com/",
+  },
+  {
+    label: "Shadcn/ui",
+    href: "https://ui.shadcn.com/",
+  },
+];
+
+export default function Footer() {
+  return (
+    <footer className="mt-6 flex flex-col items-center gap-6">
+      <p className="leading-7 [&:not(:first-child)]:mt-6">
+        Heavily inspired by <Link className="underline text-blue-500" href="https://github.com/Georgegriff/react-dnd-kit-tailwind-shadcn-ui">react-dnd-kit-tailwind-shadcn-ui</Link>.✨
+      </p>
+      <ul className="flex items-center justify-center">
+        {footerLinks.map((link) => (
+          <li key={link.href}>
+            <Button variant="link" asChild className="scroll-m-20 text-xl font-semibold tracking-tight">
+              <a href={link.href}>{link.label}</a>
+            </Button>
+          </li>
+        ))}
+      </ul>
+    </footer>
+  );
+}
