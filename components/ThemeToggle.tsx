7e6f5c623e90785df380f87025bc4ab643486829
Mehrdad Rafiee <rafiee.mehrdad@gmail.com>
2025-11-19 19:05:45 -0800

    adds view transition theme change

--- components/ThemeToggle.tsx
+++ components/ThemeToggle.tsx
@@ -9,8 +9,19 @@ import { Button } from "@/components/ui/button"
 export function ThemeToggle() {
   const { theme, setTheme } = useTheme()
 
+  const toggleTheme = () => {
+    const switchTheme = () => setTheme(theme === 'dark' ? 'light' : 'dark');
+
+    if (!document.startViewTransition) {
+      switchTheme();
+      return;
+    }
+
+    document.startViewTransition(switchTheme);
+  };
+
   return (
-    <Button className="cursor-pointer" variant="outline" size="icon" onClick={() => setTheme(theme === "dark" ? "light" : "dark")}>
+    <Button className="cursor-pointer" variant="outline" size="icon" onClick={toggleTheme}>
       {theme === "dark" ? <SunIcon size={16} /> : <MoonIcon size={16} />}
       <span className="sr-only">Toggle theme</span>
     </Button>
